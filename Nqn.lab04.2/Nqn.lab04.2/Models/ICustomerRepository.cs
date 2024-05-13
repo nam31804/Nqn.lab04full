b3cab371b1dbc8de319ef1bb2ac65a5a3ae43c83
nam31804 <nam31804@gmail.com>
2024-05-13 22:34:24 +0700

    full

---
+++ Nqn.lab04.2/Nqn.lab04.2/Models/ICustomerRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Nqn.lab04._2.Models
+{
+    public class ICustomerRepository
+    {
+        //khai báo phương thức lấy danh sách khách hàng
+        IList<Customer> GetCustomers();
+        //khai báo phương thức lấy khách hàng
+        Customer GetCustomer(string customerId);
+        //khai báo phương thức thêm khách hàng
+        void AddCustomer(Customer cus);
+        //khai báo phương thức cập nhật khách hàng
+        void UpdateCustomer(Customer cus);
+        //khai báo phương thức tìm kiếm khách hàng
+        IList<Customer> SearchCustomer(string name);
+        //khai báo phương thức xóa khách hàng
+        void DeleteCustomer(Customer cus);
+    }
+}
